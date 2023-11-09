2cbef7da45a1166ccf1a9e80700226e6b48e391b
antonio <antonio@DESKTOP-5RQ335U>
2023-11-09 13:44:41 +0100

    Relacion de ejercicio de vectores (pruebas de git, mas bien)
    Tras incontables intentos de gitear lo giteable, ahi vamos de nuevo,
    cual burro a la zanahoria

---
+++ ejercicioVectores/src/ejercicioVectores.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        : ejerciciosVectores.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "vectores.h"
+#define TAM 30
+
+int main(void) {
+
+	int longitud;
+	int vectorE [TAM];
+	//float vectorF [TAM];
+
+	puts("Dame longitud");
+	longitud=longitud_vector(TAM);
+	pedir_vectorE(vectorE, longitud);
+	rotar_der_vector(vectorE, longitud);
+	mostrar_vectorE(vectorE, longitud);
+
+	return EXIT_SUCCESS;
+}
