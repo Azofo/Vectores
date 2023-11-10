ba7ff36be0f38af24d851a01990d937966a5e294
antonio <antonio@DESKTOP-5RQ335U>
2023-11-10 10:12:00 +0100

    cambio 10

--- vectores/vectores.h
+++ vectores/vectores.h
@@ -7,6 +7,8 @@
 
 #ifndef VECTORES_H_
 #define VECTORES_H_
+
+
 int longitud_vector(int tam);
 /*Procedimiento que pide un vector por tclado*/
 void pedir_vectorE(int vector[], int tam);
@@ -19,8 +21,6 @@ void mostrar_intervalo_vector(float vector[],int tam, float low, float high);
 void rotar_izq_vector(int vector[],int tam);
 void rotar_der_vector(int vector[],int tam);
 void mostrar_Primero_Vector(int vector[],int tam, int marca);
-
-
 
 
 #endif /* VECTORES_H_ */
